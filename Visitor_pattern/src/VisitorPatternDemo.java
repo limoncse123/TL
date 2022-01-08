27bde11fb3cbd154a84ea962e19e9e942c6a1071
tanjillimon04 <tanjillimon04@gmail.com>
2022-01-08 20:56:22 +0600

    upload all file
    6th commit

---
+++ Visitor_pattern/src/VisitorPatternDemo.java
@@ -0,0 +1,13 @@
+
+public class VisitorPatternDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		      ComputerPart computer = new Computer();
+		      computer.accept(new ComputerPartDisplayVisitor());
+		   }
+	}
+
+
